fcfc7033c91cdb1758a138b34f05f5fbe8eeae88
Amr Elsayed <amr11682@hotmail.com>
2016-08-04 15:27:28 +0200

    .

---
+++ install.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+echo "Installing freeglut..."
+sudo apt-get install freeglut3-dev
+
+echo "Installing SDL..."
+sudo apt-get install libsdl1.2-dev libsdl-image1.2-dev
---
+++ src/game.h
@@ -0,0 +1,19 @@
+#ifndef __GAME_H__
+#define __GAME_H__
+
+class Level{
+
+    public:
+        int baseWall[22];
+        int pipe[22];
+};
+
+class Mario{
+    public:
+        int mariox;
+        int marioy;
+        int base;
+};
+
+#endif /* !__GAME_H__ */
+
